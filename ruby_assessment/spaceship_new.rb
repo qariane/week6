abcffa548fbe2c899c88843f9d3f74c72de78c95
Ariane Quenum <quenumariane@gmail.com>
2017-05-18 12:26:13 -0500

    Update class

--- ruby_assessment/spaceship_new.rb
+++ ruby_assessment/spaceship_new.rb
@@ -1,16 +1,27 @@
 class Spaceship
+	attr_accessor :name
+	attr_reader  :speed
 
 	def initialize(name, speed)
 		@name = name
-		@speed = speed  
+		@speed = speed 
+		
 	end 
 
 	def show
    puts "#{@name} has a max speed of #{@speed} miles per hour"
     end
+
      def ship
     puts "#{@name} has a max speed of #{@speed} miles per hour"
      end
+
+     def state_of_shield 
+      puts "To disable shield turn it off "
+      puts "To enable shield turn it on"
+     end
+
+     
 end
 
 
@@ -26,3 +37,4 @@ spaceship = Spaceship.new("USS Enterprise", 200000)
 spaceship.show
 spaceship = Spaceship.new("HMS Andromeda" , 108277)
 spaceship.ship
+spaceship.state_of_shield

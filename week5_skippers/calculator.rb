72827cfe5c5ba16bbdbd11531964340d16633da0
Ariane Quenum <quenumariane@gmail.com>
2017-05-22 10:42:50 -0500

    Perform calculation and return

--- week5_skippers/calculator.rb
+++ week5_skippers/calculator.rb
@@ -17,3 +17,33 @@ p calculator(3, '-', 4)
 p calculator(3, '*', 4)
 
 p calculator(3, '/', 4)
+
+#user interface
+puts " Welcome to your free online calculator. Everybody needs a calculator at some point."
+
+
+def recurs_calc
+  puts "Please enter two digits separated by operation you would like to calculate. Type 'done' to exit."
+  input = gets.chomp.split(' ')
+
+  if input[1] == "+"
+    puts input[0].to_i + input[2].to_i
+    recurs_calc
+  elsif input[1] == "-"
+    puts input[0].to_i - input[2].to_i
+    recurs_calc
+  elsif input[1] == "*"
+    puts input[0].to_i * input[2].to_i
+    recurs_calc
+  elsif input[1] == '/'
+    puts input[0].to_f / input[2].to_i
+    recurs_calc
+  elsif input[0] == 'done'
+    exit
+  else
+    puts 'please enter two numbers with SPACES i.e. 1 + 1 then enter'
+    recurs_calc
+  end
+end
+recurs_calc 
+	
